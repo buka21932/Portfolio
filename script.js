db9a92f0d67581f4bb5f74af3013b3dcd7bba282
buka21932 <shadowlineoff@gmail.com>
2024-11-29 20:41:48 +0300

    script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-//Добавляет плавную прокрутку страницы
-function scrollToSelection(selectionId) {
-    const selection = document.getElementById(selectionId)
-    selection.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
-}
